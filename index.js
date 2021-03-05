f13c11bab3fc58714af9dc7acf52718429ca24bf
nghiahsgs <nghiahsgs@gmail.com>
2021-03-05 14:46:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
 const find_index = (array_a,tieu_chi,value)=>{
-  for(let i=0;i<array_a.length;i++){
-    if(array_a[i][tieu_chi] == value){
-      return i
+    for(let i=0;i<array_a.length;i++){
+        if(array_a[i][tieu_chi] == value){
+            return i
+        }
     }
-  }
 }
 
-const gom_array = (List_obj,tieu_chi)=>{
-  list_kq = []
-  List_obj.forEach(obj=>{
-    list_tieu_chi = list_kq.map(e => e['camp_id'])
-    if(!list_tieu_chi.includes(obj[tieu_chi])){
+// index = find_index(List_obj,tieu_chi,obj[tieu_chi])
 
-      list_tieu_chi = [...list_tieu_chi,obj[tieu_chi]]
-      list_kq = [...list_kq,obj]
+const gom_array = (List_obj,tieu_chi,tieu_chi_cong_don)=>{
+    list_kq = []
+    List_obj.forEach(obj=>{
+        list_tieu_chi = list_kq.map(e => e[tieu_chi])
 
-    }else{
-      // da co => update
-      index = find_index(List_obj,"camp_id",obj['camp_id'])
-      list_kq[index]['cost'] += obj['cost']
-    }
+        if(!list_tieu_chi.includes(obj[tieu_chi])){
 
-    
-  })
-  // console.log(list_kq)
-  return list_kq
+            list_tieu_chi = [...list_tieu_chi,obj[tieu_chi]]
+            list_kq = [...list_kq,obj]
+
+        }else{
+            // da co => update
+            // index = find_index(List_obj,tieu_chi,obj[tieu_chi])
+            index = find_index(list_kq,tieu_chi,obj[tieu_chi])
+
+            // console.log('-------------')
+            // console.log('index',index)
+            // console.log('list_kq',list_kq)
+            // console.log('list_kq[index]',list_kq[index])
+            list_kq[index][tieu_chi_cong_don] += obj[tieu_chi_cong_don]
+        }
+
+
+    })
+    // console.log(list_kq)
+    return list_kq
 }
 
+//
 List_obj = [
-  {
-      "user_id": 70,
-      "camp_name": "132-the rivana bình dương - 1000k",
-      "camp_id": "12203261901",
-      "cost": 4710459,
-  },
-  {
-      "user_id": 70,
-      "camp_name": "132-the rivana bình dương - 1000k",
-      "camp_id": "12203261901",
-      "cost": 46853,
-  },
-  {
-      "user_id": 70,
-      "camp_name": "132-the rivana bình dương - 1000k",
-      "camp_id": "12203261902",
-      "cost": 91332
-  }
+    {
+        "user_id": 70,
+        "camp_name": "132-the rivana bình dương - 1000k",
+        "camp_id": "12203261901",
+        "cost": 1,
+    },
+    {
+        "user_id": 70,
+        "camp_name": "132-the rivana bình dương - 1000k",
+        "camp_id": "12203261902",
+        "cost": 2,
+    },
+    {
+        "user_id": 70,
+        "camp_name": "132-the rivana bình dương - 1000k",
+        "camp_id": "12203261901",
+        "cost": 10
+    }
 ]
-tieu_chi = "camp_id"
-
-List_obj_2 = gom_array(List_obj,tieu_chi)
+tieu_chi = "camp_id" // tieu chi de gom lai
+tieu_chi_cong_don = "cost"
+List_obj_2 = gom_array(List_obj,tieu_chi,tieu_chi_cong_don)
 console.log(List_obj_2)
